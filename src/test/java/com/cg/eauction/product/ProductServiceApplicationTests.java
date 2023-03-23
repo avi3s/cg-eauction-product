653b9564a5c57b16ea8efc30db469e7ca470da43
avi3s <avirup.pal@gmail.com>
2023-03-23 20:07:19 +0530

    first commit

---
+++ src/test/java/com/cg/eauction/product/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.eauction.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
